f54489c05be39e63524f933e243795d92241310c
shadowhumter_ m <Mary@DESKTOP-D2B9OOF>
2022-01-30 03:27:41 +0200

    added hero

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const btnMenu = document.querySelector('.header__btn-menu');
+const menu = document.querySelector('.header__nav');
+
+btnMenu.addEventListener('click', function() {
+  btnMenu.classList.toggle('header__btn-menu--active');
+  menu.classList.toggle('header__nav--open');
+});
